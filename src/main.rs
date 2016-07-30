13edf90cba0fb70367295c36668ad3181dbece29
BuildTools <unconfigured@null.spigotmc.org>
2016-07-30 14:42:49 -0500

    Start

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+
+use std::fs;
+
+fn main(){
+
+    let config_dir = fs::read_dir("./configs/").unwrap();
+    let configs: u8 = 0;
+
+    for f in config_dir{
+        configs = 1 + configs;
+    }
+
+    println!("There are {0} in the config folder.", configs);
+
+
+}
