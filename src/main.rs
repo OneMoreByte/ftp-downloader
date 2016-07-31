65f4e0e17fb5e7d0929a417e99279543befe7504
BuildTools <onemorebyte13@gmail.com>
2016-07-31 11:36:49 -0500

    Config

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 
 use std::fs;
 
-fn main(){
+
+
+fn load_configs()
+{
 
     let config_dir = fs::read_dir("./configs/").unwrap();
-    let configs: u8 = 0;
+    {fs::create_dir("./configs/"); fs::read_dir("./configs");
+
+    let mut configs: u8 = 0;
 
     for f in config_dir{
         configs = 1 + configs;
     }
 
     println!("There are {0} in the config folder.", configs);
+
+}
+
+
+
+fn main(){
+
+    load_configs();
 
 }
