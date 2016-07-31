7907964c307b7604445874ae0baa7ea518b41e54
BuildTools <onemorebyte13@gmail.com>
2016-07-31 11:38:50 -0500

    Config

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
-
 use std::fs;
+use std::fs::OpenOptions;
 
+/// Makes a directory specified
+fn mk_dir(d: &str) -> std::io::Result<()> {
+        try!(fs::create_dir_all(d));
+        Ok(())
+}
 
+/// Loads all configs into the folder
+fn load_configs() {
 
-fn load_configs()
-{
+    mk_dir("./configs");
 
     let config_dir = fs::read_dir("./configs/").unwrap();
-    {fs::create_dir("./configs/"); fs::read_dir("./configs");
-
     let mut configs: u8 = 0;
 
     for f in config_dir{
-        configs = 1 + configs;
+        configs += 1;
+        let d = f.file_name();
+        println!("{:?}", d);
+        //let conf = OpenOptions::new().read(true).write(true).open(f);
     }
 
-    println!("There are {0} in the config folder.", configs);
-
+    println!("There were {0} files in the config folder.", configs);
 }
 
-
-
 fn main(){
-
     load_configs();
 
 }
